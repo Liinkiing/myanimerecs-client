13534fff6c57c1cf677ffc5ddec539960e636189
Omar Jbara <omar.jbara2@gmail.com>
2019-10-31 02:04:57 +0100

    - Made animations faster

--- src/components/anime/item/framer.ts
+++ src/components/anime/item/framer.ts
@@ -30,21 +30,21 @@ export const variants = {
     selected: {
       opacity: 1,
       borderRadius: 0, overflowY: ('overlay' as any), transition: {
-        staggerChildren: 0.5
+        staggerChildren: 0.2
       }
     }
   } as Variants,
   AnimeItemInnerImg: {
     show: {position: 'absolute', filter: 'blur(0)'},
-    selected: {position: 'fixed', filter: 'blur(15px)', transition: { delay: 0 }}
+    selected: {position: 'fixed', filter: 'blur(15px)'}
   } as Variants,
   AnimeBanner: {
     show: {opacity: 0, transition: {delay: 0}, display: 'none'},
-    selected: {opacity: 1, transition: {delay: 2}, display: 'block'}
+    selected: {opacity: 1, transition: {delay: 1.25}, display: 'block'}
   } as Variants,
   BackIcon: {
     show: {opacity: 0, x: -40, display: 'none', transition: {delay: 0}},
-    selected: {opacity: 1, x: 0, display: 'inline-flex', transition: {delay: 2}}
+    selected: {opacity: 1, x: 0, display: 'inline-flex', transition: {delay: 1}}
   } as Variants,
   AnimeTitle: {
     show: {
